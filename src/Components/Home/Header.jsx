ffc14c74cb83e5a169def9903a907968611d54f7
Pol Seznec <seznecpol@gmail.com>
2022-01-31 19:00:21 +0100

    TEST - Gsap Animation with useRef & fowardRef

--- src/Components/Home/Header.jsx
+++ src/Components/Home/Header.jsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import { gsap } from "gsap";
 
 //Components Import
 import { Colunn, FullScreenContainer, Row } from "../../Components/Container";
@@ -6,11 +7,24 @@ import { H1 } from "../../Components/Text";
 //Assets Import
 
 function Header() {
+  const [toggel, setToggel] = React.useState(false);
+
+  const changeState = () => {
+    setToggel(!toggel);
+  };
+  const cardRef = React.useRef(null);
+
+  React.useEffect(() => {
+    toggel
+      ? gsap.to(cardRef.current, { scale: 1.5, rotate: 360, duration: 1.2 })
+      : gsap.to(cardRef.current, { scale: 1, rotate: 0, duration: 1.2 });
+  }, [toggel]);
+
   return (
     <FullScreenContainer className="header">
       <Colunn>
         <Row>
-          <H1 text="Pute" />
+          <H1 text="Pute" onClick={changeState} ref={cardRef} />
         </Row>
       </Colunn>
     </FullScreenContainer>
--- src/Components/Text.jsx
+++ src/Components/Text.jsx
@@ -3,23 +3,23 @@ import * as React from "react";
 // functions Imported
 import { tClassName as TCL } from "../utility/Functions";
 
-export function H1({ className, text }, { ...props }) {
+export const H1 = React.forwardRef(({ className, text, ...props }, ref) => {
   return (
-    <span className={TCL("h1", className)} {...props}>
+    <span className={TCL("h1", className)} ref={ref} {...props}>
       {text}
     </span>
   );
-}
+});
 
-export function H2({ className, text }, { ...props }) {
+export const H2 = ({ className, text, ...props }) => {
   return (
     <span className={TCL("h2", className)} {...props}>
       {text}
     </span>
   );
-}
+};
 
-export function H3({ className, text }, { ...props }) {
+export function H3({ className, text, ...props }) {
   return (
     <span className={TCL("h3", className)} {...props}>
       {text}
@@ -27,21 +27,21 @@ export function H3({ className, text }, { ...props }) {
   );
 }
 
-export function H4({ className, text }, { ...props }) {
+export function H4({ className, text, ...props }) {
   return (
     <span className={TCL("h4", className)} {...props}>
       {text}
     </span>
   );
 }
-export function H5({ className, text }, { ...props }) {
+export function H5({ className, text, ...props }) {
   return (
     <span className={TCL("h5", className)} {...props}>
       {text}
     </span>
   );
 }
-export function H6({ className, text }, { ...props }) {
+export function H6({ className, text, ...props }) {
   return (
     <span className={TCL("h6", className)} {...props}>
       {text}
@@ -49,7 +49,7 @@ export function H6({ className, text }, { ...props }) {
   );
 }
 
-export function Body({ className, text }, { ...props }) {
+export function Body({ className, text, ...props }) {
   return (
     <span className={TCL("body", className)} {...props}>
       {text}
