d7ea5673dfcee1d1a88e144f7598f91a639f8142
Pol Seznec <seznecpol@gmail.com>
2022-01-31 15:12:31 +0100

    Add - H2 in header comp

--- src/Components/Home/Header.jsx
+++ src/Components/Home/Header.jsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 //Components Import
 import { Colunn, FullScreenContainer, Row } from "../../Components/Container";
-import { H1 } from "../../Components/Text";
+import { H1, H2 } from "../../Components/Text";
 //Assets Import
 
 function Header() {
@@ -12,6 +12,7 @@ function Header() {
         <Row>
           <H1 text="Pol" />
           <H1 text="Seznec" />
+          <H2 text="lorems in supm" />
         </Row>
       </Colunn>
     </FullScreenContainer>
